e90f951852cde2933872c4166cacf1235823aad5
Heeralal Kumar Sahni <heeralalkumarheera@gmail.com>
2026-04-09 12:11:32 +0530

    update code

--- Daily_problem/Segregate_0s_and_1s.py
+++ Daily_problem/Segregate_0s_and_1s.py
@@ -18,29 +18,20 @@
 
 //gfg solution
 
-class Solution {
-  public:
-    void segregate0and1(vector<int> &arr) {
-        // code here
-        int n=arr.size();
-        int i=0;
-        int j=n-1;
-        while(i<j)
-        {
-            while(i<n && arr[i]==0)
-            {
-                i++;
-            }
-            while(j>=0 && arr[j]==1)
-            {
-                j--;
-            }
-            if(i<j)
-            {
-                swap(arr[i],arr[j]);
-                i++;
-                j--;
-            }
-        }
-    }
-};
+class Solution:
+    def segregate0and1(self, arr):
+        left = 0
+        right = len(arr) - 1
+
+        while left < right:
+
+            if arr[left] == 0:
+                left += 1
+
+            elif arr[right] == 1:
+                right -= 1
+
+            else:
+                arr[left], arr[right] = arr[right], arr[left]
+                left += 1
+                right -= 1
